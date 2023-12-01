ded1ede70a0a285b0aa5c697b2a21dc50435c23d
Denys Ponomarov <tenstepsint@gmail.com>
2023-12-01 15:43:08 +0200

    models User and Comment

--- comments_app/models.py
+++ comments_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    home_page = models.URLField(blank=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
