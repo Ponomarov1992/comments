6095bf9a58868d5bec75c83fbd3dcd89ebf5010d
Denys Ponomarov <tenstepsint@gmail.com>
2023-12-02 18:55:05 +0200

    fix bag in path

--- comments_app/urls.py
+++ comments_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
 from .views import CommentListCreateView
+from .views import CommentListCreateAPIView
+
+
+app_name = 'comments_app'
 
 urlpatterns = [
+    path("api/comments/", CommentListCreateAPIView.as_view(), name='comment-list-create'),
     path("api/comments/", CommentListCreateView.as_view(), name="comment-list-create"),
     path("comments/", CommentListCreateView.as_view(), name="comment-list-create"),
     path("", CommentListCreateView.as_view(), name="home"),
