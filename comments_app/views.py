556ab7f09a0d4e7b1164596b6dec3b4fe5748964
Denys Ponomarov <tenstepsint@gmail.com>
2023-12-02 17:18:32 +0200

    make format

--- comments_app/views.py
+++ comments_app/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
+
 from .models import Comment
 from .serializers import CommentSerializer
+
 
 class CommentListCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
