5882d4d556540e3ab6b37ade2c84b3148681054e
Denys Ponomarov <tenstepsint@gmail.com>
2023-12-02 14:39:45 +0200

    views

--- comments_app/views.py
+++ comments_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Comment
+from .forms import CommentForm
 
-# Create your views here.
+def index(request):
+    comments = Comment.objects.filter(parent_comment__isnull=True).order_by('-created_at')
+    return render(request, 'comments_app/index.html', {'comments': comments})
+
+def add_comment(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = CommentForm()
+    return render(request, 'comments_app/add_comment.html', {'form': form})
